c106ac26d255f3440499a7a96c8ed88e3f1cf81d
juliscapucin <jscapucin@gmail.com>
2025-11-21 14:58:39 +0000

    add cover page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,16 @@ export default async function Home() {
 	const { session } = await getUserSession()
 
 	return (
-		<PageWrapper pageName='home' sessionData={session}>
-			<Search />
-			<FeaturedArtists artists={simpleArtists} />
-		</PageWrapper>
+		<>
+			<div className='bg-primary min-h-screen w-full text-secondary flex items-center justify-center flex-col'>
+				<h1 className='heading-display'>Smalls London</h1>
+				<p className='text-body mt-4'>Website coming soon</p>
+			</div>
+			{/* <PageWrapper pageName='home' sessionData={session}>
+				<Search />
+				<FeaturedArtists artists={simpleArtists} />
+			</PageWrapper> */}
+		</>
 	)
 }
+;``
